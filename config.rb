c73d43b75bb54ded8a35d48e72214fedef243396
jfaretta <justinfaretta@gmail.com>
2018-04-18 00:43:40 -0500

    init push

---
+++ config.rb
@@ -0,0 +1,24 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path = "C:/Users/justi/Desktop/uscca/landing-page"
+css_dir = "css"
+sass_dir = "css/application"
+images_dir = "img"
+generated_images_path = "C:/Users/justi/Desktop/uscca/landing-page/generatedimages"
+javascripts_dir = "javascripts"
+fonts_dir = "fonts"
+
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass css/application scss && rm -rf sass && mv scss sass
